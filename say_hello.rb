2413a96cc9340d9ef7e9f192b0c4d25a8a303630
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-15 03:29:22 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
-def say_hello ( greeting , person = "Programmer")
+def say_hello ( greeting , person = " Ruby Programmer")
   puts greeting  + " "+ person
 end
   say_hello("Hello", "Karlie Kloss!")
